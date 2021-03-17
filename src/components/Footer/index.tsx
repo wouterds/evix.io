0a58a3f3f933ea5faa5c200311ab8f9cc7ec25fb
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-03-17 19:46:32 +0100

    :sparkles: Footer link to status page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link';
+
 import styles from './styles.module.css';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    &copy; {new Date().getFullYear()} Evix - BE 0745.964.642
+    <p>&copy; {new Date().getFullYear()} Evix - BE 0745.964.642</p>
+    <nav>
+      <Link href="/status">
+        <a>status</a>
+      </Link>
+    </nav>
   </footer>
 );
 
