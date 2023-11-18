f84c50ca7c2975a830f02ce16cd7635621c6a53b
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-11-18 18:23:05 +0100

    perf: prefetch on hover

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ const Footer = () => (
   <footer className="border-t border-dashed border-zinc-900 dark:border-zinc-100 py-3 flex justify-between">
     <p>&copy; {new Date().getFullYear()} Evix - BE 0745.964.642</p>
     <nav>
-      <Link to="/status">status</Link>
+      <Link to="/status" prefetch="intent">
+        status
+      </Link>
     </nav>
   </footer>
 );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,15 @@ import { Link } from '@remix-run/react';
 const Header = () => (
   <header className="border-t border-b border-dashed border-zinc-900 dark:border-zinc-100 py-3">
     <ul className="flex justify-end items-center gap-2.5">
-      <Link to="/">/</Link>
-      <Link to="/about">about</Link>
-      <Link to="/contact">contact</Link>
+      <Link to="/" prefetch="intent">
+        /
+      </Link>
+      <Link to="/about" prefetch="intent">
+        about
+      </Link>
+      <Link to="/contact" prefetch="intent">
+        contact
+      </Link>
     </ul>
   </header>
 );
