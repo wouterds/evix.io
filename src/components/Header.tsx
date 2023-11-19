27313d8f44db48b09cfdc6e35fa593143718746a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-11-19 13:43:50 +0100

    fix: missing li tags

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,21 @@ import { Link } from '@remix-run/react';
 const Header = () => (
   <header className="border-t border-b border-dashed border-zinc-900 dark:border-zinc-100 py-3">
     <ul className="flex justify-end items-center gap-2.5">
-      <Link to="/" prefetch="intent">
-        /
-      </Link>
-      <Link to="/about" prefetch="intent">
-        about
-      </Link>
-      <Link to="/contact" prefetch="intent">
-        contact
-      </Link>
+      <li>
+        <Link to="/" prefetch="intent">
+          /
+        </Link>
+      </li>
+      <li>
+        <Link to="/about" prefetch="intent">
+          about
+        </Link>
+      </li>
+      <li>
+        <Link to="/contact" prefetch="intent">
+          contact
+        </Link>
+      </li>
     </ul>
   </header>
 );
